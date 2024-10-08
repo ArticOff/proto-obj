e48fa140846fca70108a4117bf07561d80052196
Artic <artic.admisoffi@gmail.com>
2024-10-08 21:06:19 +0200

    Update __init__.py

--- proto/__init__.py
+++ proto/__init__.py
@@ -1,5 +1,6 @@
 from typing import Callable
 from types import MethodType
+from copy import deepcopy
 
 # Fonction permettant de servir d'initiateur aux objets.
 def init(self, *args, **kwargs) -> None:
@@ -9,13 +10,31 @@ def init(self, *args, **kwargs) -> None:
         setattr(self, key, value)
     return
 
-# Fonction permettant de créer des méthodes dans l'objet au travers d'un décorateur.
-def decorator(self: object):
-    def wrapper(function: Callable):
-        setattr(self, function.__name__, MethodType(function, self))
-        return function
-    return wrapper
+# Fonction gérant l'entrée du with.
+def wEnter(self):
+    return self
+
+# Fonction gérant la sortie du with.
+def wExit(self, executionType, executionValue, traceback):
+    return self
+
+# Fonction gérant les appels de l'objet.
+def decorator(self, *args, **kwargs):
+    if hasattr(args[0], "__call__"):
+        setattr(self, args[0].__name__, MethodType(args[0], self))
+    else:
+        o = deepcopy(self)
+        o.new(*args, **kwargs)
+        return o
+    return self
 
 # Fonction permettant de créer les objets.
 def proto(name: str, methods: dict[str, Callable] = {}) -> type:
-    return type(name, (object,), {"__init__": init, "__call__": decorator, **methods})
+    o = type(name, (object,), {
+        "__init__": init,
+        "__call__": decorator,
+        "__enter__": wEnter,
+        "__exit__": wExit,
+        **methods
+    })
+    return o()
